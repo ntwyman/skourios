6a0682fdf52ea4c59b24bf16e350425d9091311d
Nick Twyman <nick.twyman@acdsglobal.com>
2025-02-15 11:08:38 +0000

    chore: setup initial environment and build tasks.
    NOTE: Dependencies don't resolve properly between tasks so you need to manually run each task in turn ATM.

---
+++ crates/root_task/src/main.rs
@@ -0,0 +1,10 @@
+#![no_std]
+#![no_main]
+
+use sel4_root_task::{Never, root_task};
+
+#[root_task]
+fn main(_bootinfo: &sel4::BootInfoPtr) -> sel4::Result<Never> {
+    sel4::debug_println!("Hello, Skourios!");
+    sel4::init_thread::suspend_self()
+}
